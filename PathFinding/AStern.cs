e3fe2b1627527f68368d124d0a766bfde1a9582c
CaptianMaulber <breuninj+commit@gmail.com>
2024-04-12 22:47:06 +0200

    init a* and bestagon grid

---
+++ PathFinding/AStern.cs
@@ -0,0 +1,14 @@
+using IDontWantToDoThisTwice.DataStructures;
+
+namespace IDontWantToDoThisTwice.PathFinding {
+public class AStern<T> where T : IPathNode<T>{
+    private BinaryHeap<T> _openSet;
+    
+    public AStern() {
+    }
+    
+    public void FindPath(T start, T end) {
+        
+    }
+}
+}
---
+++ PathFinding/Bestagon.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+namespace IDontWantToDoThisTwice.PathFinding {
+public class Bestagon {
+   public int FCost { get; set; }
+   public int GCost { get; set; }
+   public int HCost { get; set; }
+
+   public Bestagon(Vector2 position) { }
+}
+}
